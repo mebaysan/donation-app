b233e1e2ac617f17d2475d747a31b376d4c05e99
mebaysan <menesbaysan@gmail.com>
2023-02-23 10:16:08 +0300

    STATIC_URL

--- src/donation/settings.py
+++ src/donation/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/django-static/" # for proxy purposes
 STATICFILES_DIRS = [BASE_DIR / "assets"]
 STATIC_ROOT = BASE_DIR / "static"
 
