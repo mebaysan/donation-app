1be6c3188f36a6250567c45779237bcdfc1e0d0e
mebaysan <menesbaysan@gmail.com>
2023-02-23 13:35:31 +0300

    update settings.py

--- src/donation/settings.py
+++ src/donation/settings.py
@@ -111,11 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "tr-TR"
+LANGUAGE_CODE = "en-EN"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Istanbul"
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
