afcc583ae49ac1d0ce38ef09f3419a975afba7bc
mebaysan <menesbaysan@gmail.com>
2023-02-23 12:40:38 +0300

    CSRF_TRUSTED_ORIGINS added

--- src/donation/config_dev.py
+++ src/donation/config_dev.py
@@ -15,6 +15,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
--- src/donation/config_prod.py
+++ src/donation/config_prod.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
 
-# CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
