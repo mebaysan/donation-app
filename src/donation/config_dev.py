09c2f3893f2e8053911abe314750382f34875f55
mebaysan <menesbaysan@gmail.com>
2023-03-12 11:22:04 +0300

    config_dev fix

--- src/donation/config_dev.py
+++ src/donation/config_dev.py
@@ -38,9 +38,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 # KUVEYTTURK CONF
 KUVEYTTURK_CONF = {
-    "store_no": "57902",
-    "customer_no": "97228291",
-    "username": "TEPKVT2021",
+    "store_no": "57902",  # 57902 || 496
+    "customer_no": "97228291",  # 97228291 || 400235
+    "username": "TEPKVT2021",  # TEPKVT2021 || apitest
     "password": "api123",
     "ok_url": "http://127.0.0.1:8000/api/payment-success/",
     "fail_url": "http://127.0.0.1:8000/api/payment-fail/",
@@ -49,9 +49,9 @@ KUVEYTTURK_CONF = {
 }
 # TEST CARD FOR KUVEYTTURK
 # Test Kart Bilgileri
-# Kart No: 5188 9619 3919 2544
-# CVV2: 929
-# Expirydate: 06/25
+# Kart No: 5188 9619 3919 2544 || 4033 6025 6202 0327
+# CVV2: 929 || 861
+# Expirydate: 06/25 || 01/30
 
 # JWT CONF
 TOKEN_LIFETIME_HOURS = 5
