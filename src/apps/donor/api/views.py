45bd1f1453181acc91f14705c4fa31e59245b92e
mebaysan <menesbaysan@gmail.com>
2024-03-21 19:22:00 +0300

    permission_classes implemented in donor.api.views to allow unauthenticated users

--- src/apps/donor/api/views.py
+++ src/apps/donor/api/views.py
@@ -14,25 +14,29 @@ class DonationCategoryListAPIView(ListAPIView):
     queryset = (
         DonationCategory.objects.filter(is_published=True).order_by("order").all()
     )
+    permission_classes = []
 
 
 class DonationCategoryRetrieveAPIView(RetrieveAPIView):
     serializer_class = DonationCategoryDetailsSerializer
     queryset = DonationCategory.objects.filter(is_published=True).all()
     lookup_field = "pk"
+    permission_classes = []
 
 
 class DonationItemListAPIView(ListAPIView):
     serializer_class = DonationItemSerializer
     queryset = DonationItem.objects.filter(is_published=True).all()
-
+    permission_classes = []
 
 class DonationItemRetrieveAPIView(RetrieveAPIView):
     serializer_class = DonationItemSerializer
     queryset = DonationItem.objects.filter(is_published=True).all()
     lookup_field = "pk"
+    permission_classes = []
 
 
 class BankListAPIView(ListAPIView):
     serializer_class = BankSerializer
     queryset = Bank.objects.filter(is_published=True).order_by("order").all()
+    permission_classes = []
